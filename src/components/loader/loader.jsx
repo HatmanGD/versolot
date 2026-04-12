d24bcd697bbe2c3dc90aa3108468297ab5bd4cd4
~ Gaia WindWave ~ <crystalfranks18@hotmail.com>
2026-04-12 05:49:45 -0400

    Update loader.jsx

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -217,7 +217,7 @@ class LoaderComponent extends React.Component {
             >
                 <div className={styles.container}>
                     <div className={styles.blockAnimation}>
-                        <img src={sprunki} alt="A Sprunki!" />
+                        <img src={sprunki} alt="A Sprunki!" width="50%"/>
                     </div>
                     <div className={styles.title}>
                         {mainMessages[this.props.messageId]}
